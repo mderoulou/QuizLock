e3b5f901885bf22919ca37b7e2cc046defff330c
Maxime Deroulou <maxime.deroulou@epitech.eu>
2021-09-24 18:31:59 +0200

    [+] init.sh & [~] &> /dev/null autorun

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip3 install pygame python-xlib
-python3 script.py
+pip3 install pygame python-xlib &> /dev/null
+python3 QuizLock/script.py &> /dev/null
---
+++ init.sh
@@ -0,0 +1,1 @@
+cp autorun.sh ..
